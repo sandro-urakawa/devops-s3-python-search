924af500f6434109645cd6480634c023589eb0c7
Alessandro Kenji Urakawa <sandro.urakawa@gmail.com>
2022-10-31 00:31:19 +0000

    Add ignore-case parameter in search-s3.py

--- python/search-s3.py
+++ python/search-s3.py
@@ -7,11 +7,14 @@ endpoint_url = "http://localhost.localstack.cloud:4566"
 def main(argv):
     s3_bucket_name = ''
     substring = ''
+    ignore_case = False
     found = 0
-    help_text = "usage: python search-s3.py -b|--s3_bucket_name <s3_bucket_name> -s|--substring <substring>"
+    help_text = """usage: python search-s3.py -b|--s3_bucket_name <s3_bucket_name> -s|--substring <substring>
+            Optional parameters:
+                -i | --ignore-case"""
 
     try:
-        opts, args = getopt.getopt(argv,"b:s:",["s3_bucket_name=","substring="])
+        opts, args = getopt.getopt(argv,"hib:s:",["s3_bucket_name=","substring="])
     except getopt.GetoptError:
         print(help_text)
         sys.exit(2)
@@ -23,6 +26,8 @@ def main(argv):
             s3_bucket_name = arg
         elif opt in ("-s", "--substring"):
              substring = arg
+        elif opt in ("-i", "--ignore-case"):
+             ignore_case = True
 
     #verify required parameters
     if s3_bucket_name == '' or substring == '':
@@ -52,11 +57,20 @@ def main(argv):
         with open(filename, 'r') as data:
             lines = data.readlines()
             for line in lines:
-                if line.find(substring) != -1:
-                    print(substring, 'string exists in', file['Key'])
-                    print('Line Number:', lines.index(line)+1)
-                    print('Line:', line)
-                    found = found + 1
+                if (ignore_case):
+                    line_case = line.casefold()
+                    substring_case = substring.casefold()
+                    if line_case.find(substring_case) != -1:
+                        print(substring, 'string exists in', file['Key'])
+                        print('Line Number:', lines.index(line)+1)
+                        print('Line:', line)
+                        found = found + 1
+                else:
+                    if line.find(substring) != -1:
+                        print(substring, 'string exists in', file['Key'])
+                        print('Line Number:', lines.index(line)+1)
+                        print('Line:', line)
+                        found = found + 1
     if found == 0:
         print(substring, "not found in any file")
         exit()
